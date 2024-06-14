a1ab6b97f16b9685ef6e7d27df5f8a2b968690f8
VerNeu <verneu@gmx.de>
2024-06-14 20:53:16 +0200

    inital upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package FigureFamily;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Figure figure = new Cuboid (4.4564, 9.324, 4.67);
+		figure.giveAllResults(figure.length, figure.width, figure.height);
+		
+	}
+
+}
